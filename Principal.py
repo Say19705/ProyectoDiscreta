66f657c0d32af341eaf6141a6c2c681269b40729
Say19705 <54776073+Say19705@users.noreply.github.com>
2020-11-21 15:36:27 -0600

    Update Principal.py

--- Principal.py
+++ Principal.py
@@ -2,3 +2,28 @@
 #Julio marcos monzón
 #Andrés Say Agosto
 #Ayleen Rubio
+from itertools import combinations
+
+#colores=["rojo","azul","amarillo","Verde"]
+
+#combinacion = list(combinations(colores,3))
+
+#print(combinacion)
+prueba = "hola"
+listPrueba = []
+llena = ""
+for i in prueba:
+    listPrueba.append(i)
+final = set(listPrueba)
+final = sorted(final)
+print(final)
+for j in final:
+    llena += j
+print(llena)
+eliminados = len(prueba) - len(llena)
+print(eliminados)
+
+    
+
+
+
