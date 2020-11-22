d2c54b5c7a25df8790dacbecc3a2407a26936cf4
Say19705 <54776073+Say19705@users.noreply.github.com>
2020-11-22 17:41:18 -0600

    Update Principal.py

--- Principal.py
+++ Principal.py
@@ -4,30 +4,21 @@
 #Ayleen Rubio
 from itertools import combinations
 
-#colores=["rojo","azul","amarillo","Verde"]
 
-#combinacion = list(combinations(colores,3))
+def repetidos(conjunto):
+    lista = []
+    sinRepetidos = ""
+    for i in conjunto:
+        lista.append(i)
+    final = set(lista)
+    final = sorted(final)
+    for j in final:
+        sinRepetidos += j
+    return sinRepetidos
+        
 
-#print(combinacion)
-prueba = "hola"
-listPrueba = []
-llena = ""
-for i in prueba:
-    listPrueba.append(i)
-final = set(listPrueba)
-final = sorted(final)
-print(final)
-for j in final:
-    llena += j
-print(llena)
-eliminados = len(prueba) - len(llena)
-print(eliminados)
 
     
-
-
-
-
 
 def potencia(c):
     if len(c) == 0:
@@ -47,4 +38,26 @@ def combinaciones(c,n):
     return [s for s in potencia (c) if len(s) ==n]
 
 imprime_ordenado(combinaciones(["cereza", "chocolate", "fresa", "cereza"],2))
+
+
+#-------------------------------------------------------------------------
+#Permutaciones
+def permutacionesTotales(lista):
+    if len(lista) == 0:
+        return []
+    elif len(lista) == 1:
+        return [lista]
+    else:
+        temp = []
+        for i in range(len(lista)):
+            posicion = lista[i]
+            px = lista[:i] + lista[i+1:]
+            for j in permutacionesTotales(px):
+                temp.append([posicion]+j)
+        return temp
+
+data =list("abc")
+print("perm1")
+for p in permutacionesTotales(data):
+    print(p)
 
