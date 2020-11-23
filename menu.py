97ce348cb03b024cc5442da28061412c8e1ca8a8
Say19705 <54776073+Say19705@users.noreply.github.com>
2020-11-23 14:13:36 -0600

    Update menu.py

--- menu.py
+++ menu.py
@@ -14,17 +14,33 @@ print(conjuntoCo)
 conjuntoPe = conjunto
 combFlag = False
 permFlag = False
-Kseleccion = int(input("Cuantos elementos desea seleccionar del conjunto: "))
-opcion = input("Ingrese lo que desea imprimir: \n1. permutaciones \n2. combinaciones \n3. ambas \n->")
-if opcion == "1":
-    permFlag = True
-elif opcion == "2":
-    listaF = pr.elementos(conjuntoCo)
-    resultComb = pr.combinaciones(listaF, Kseleccion)
-    imprimeComb=pr.imprime_ordenado(resultComb)
-    combFlag = True
-elif opcion == 3:
-    permFlag = True
-    combFlag == True
+flag = True
+while flag:
+    try:
+        Kseleccion = int(input("Cuantos elementos desea seleccionar del conjunto: "))
+        flag = False            
+    except:
+        print("Debes ingresar un número entero, menor o igual a la cantidad de elementos del conjunto")
+
+flag = True
+while flag:
+    try:
+        
+        opcion = input("Ingrese lo que desea imprimir: \n1. permutaciones \n2. combinaciones \n3. ambas \n4. salir\n->")
+        if opcion == "1":
+            permFlag = True
+        elif opcion == "2":
+            listaF = pr.elementos(conjuntoCo)
+            resultComb = pr.combinaciones(listaF, Kseleccion)
+            imprimeComb=pr.imprime_ordenado(resultComb)
+            combFlag = True
+        elif opcion == 3:
+            permFlag = True
+            combFlag == True
+        elif opción == 4:
+            print("Gracias por usar el programa")
+            flag = False
+    except:
+        print("Has ingresado algo mal, intenta otra vez")
     
 #-----------En teoría aqui van las impresiones
